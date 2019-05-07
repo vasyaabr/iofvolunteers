23eb244dcad6273deda48b90729ee772f3e2ea43
vasyaabr <vasyaabr@gmail.com>
2019-05-07 16:32:18 +0300

    Deploy script update

--- deploy.php
+++ deploy.php
@@ -1,4 +1,9 @@
 <?php
-echo shell_exec('git pull');
-mail('vasyaabr@gmail.com', 'Deploy', 'IOF deployed');
+$result = '';
+if (isset($_POST)) {
+    $result = 'POST data: '.implode(';', $_POST)."\n";
+}
+$result .= 'Exec result: ' . shell_exec('git pull');
+mail('vasyaabr@gmail.com', 'Deploy', 'IOF deployed: ' . $result);
+echo $result;
 die();
