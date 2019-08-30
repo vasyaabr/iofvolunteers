f32c78fba6a580177e4ce4b2556c890f4a3ff252
vasyaabr <vasyaabr@gmail.com>
2019-08-30 10:10:46 +0300

    twitter bootstrap init

--- controllers/Platform.php
+++ controllers/Platform.php
@@ -6,17 +6,16 @@ class Platform {
 
     public function load() : string {
 
-        $params = ['welcome' => '<b>Welcome to the Global Orienteering Volunteer Platform!<br>
+        if (empty($_SESSION['user'])) {
+            $params = [
+                'welcome' => '<strong>Welcome to the Global Orienteering Volunteer Platform!</strong><br>
                         <br>You need to register in order to access the platform! If already registered, then sign in
-                        first...</b>'];
-
-        if (!empty($_SESSION['user'])) {
-            $params['welcome'] = '';
-            if (!empty($_SESSION['auth_done'])) {
-                $names             = explode( ' ', trim( $_SESSION['user']['name'] ) );
-                $params['welcome'] = "Welcome {$names[0]}! Be part of the Global Orienteering Volunteer Network by clicking on any of the icons below...";
-                unset( $_SESSION['auth_done'] );
-            }
+                        first...'
+            ];
+        } else if (!empty($_SESSION['auth_done'])) {
+            $names             = explode( ' ', trim( $_SESSION['user']['name'] ) );
+            $params['welcome'] = "Welcome {$names[0]}! Be part of the Global Orienteering Volunteer Network by clicking on any of the icons below...";
+            unset( $_SESSION['auth_done'] );
         }
 
         return TemplateProvider::render('index.twig', $params);
